bda9d29c61f4901b898fab9938c1aa47731b7e73
Anirudh-Kala <akala@quadragensolutions.com>
2026-05-04 23:11:55 +0530

    Add files via upload

---
+++ GST Input Party Wise Detail Report-R12-AP071 - XXAU_AP071_GST_INPUT/SOLUTION FOR REPORT.sql
@@ -0,0 +1,14 @@
+GST Input Party Wise Detail Report-R12-AP071
+
+LOOKUP - XXAUIPPL_GST UNIT_STATE_GSTNO
+
+Code|Value| Description	  | Tag				| Start Date
+142 | 724 | UTTAR PRADESH | 09AAFCI2079D1ZM | 22-12-2025
+
+
+Need to enter following entry
+
+LOOKUP - XXAU_GST UNIT_STATE_WITH_GSTNO
+
+Code |Value | Description	  | Tag				| Start Date
+724  |724   | UTTAR PRADESH   | 09AAFCI2079D1ZM | 15-04-2026
